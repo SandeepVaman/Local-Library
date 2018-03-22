4f5a1e2bf0fa016041d380ca4761639cd2816173
SandeepVaman <sandeepvbujire@gmail.com>
2018-03-22 11:58:34 +0530

    Added handlers for bookinstance controller

---
+++ express-local-library/controllers/bookinstanceController.js
@@ -0,0 +1,41 @@
+var BookInstance = require("../models/bookinstance");
+
+//Display list of all book instances
+exports.bookInstance_list = function(req, res){
+    res.send('NOT IMPLEMENTED: bookInstance_list');
+};
+
+//Display book instance details
+exports.book_instance_detail = function(req, res){
+    res.send('NOT IMPLEMENTED: book_instance_detail');
+};
+
+//Display book instance create form on GET
+exports.book_instance_create_get = function(req, res){
+    res.send('NOT IMPLEMENTED: book_instance_create_get');
+};
+
+//Display bookInstance create on POST
+exports.book_instance_create_post = function(req, res){
+    res.send('NOT IMPLEMENETD: book_instance_create_post');
+};
+
+//Display bookInstance delete form on GET
+exports.bookinstance_delete_get = function(req, res){
+    res.send('NOT IMPLEMENTED: bookinstance_delete_get');
+};
+
+//Display bookInstance delete form on POST 
+exports.bookinstance_delete_post = function(req, res){
+    res.send('NOT IMPLEMENTED: bookinsatnce_delete_post');
+};
+
+//Display bookInstance update form on GET
+exports.bookinstance_update_get = function(req, res){
+    res.send('NOT IMPLEMENTED: bookinstance_update_get');
+};
+
+//Display bookInstance update form on POST
+exports.bookinstance_update_post = function(req, res){
+    res.send('NOT implemented: bookInstance_update_post')
+};
